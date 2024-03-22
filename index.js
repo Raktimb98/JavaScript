e28685d93421d6c759b49a295ce77c138aa75d0f
Rakim Biswas <raktimb181@gmail.com>
2024-03-22 08:23:07 +0530

    Rename the file

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hi");
+let a = 20;
+let b = " hi";
+console.log(a + b);
+{
+  const a = 50;
+  console.log(a); //this will show the value of const a which is 50
+}
+console.log(typeof b);
