d2287dcc22e74a131390504c2c47a981a6003cab
Rakim Biswas <raktimb181@gmail.com>
2024-03-24 12:32:54 +0530

    New update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-console.log("hi");
-let a = 20;
-let b = " hi";
-console.log(a + b);
-{
-  const a = 50;
-  console.log(a); //this will show the value of const a which is 50
-}
-console.log(typeof b);
+// console.log("hi");
+// let a = 20;
+// let b = " hi";
+// console.log(a + b);
+// {
+//   const a = 50;
+//   console.log(a); //this will show the value of const a which is 50
+// }
+// console.log(typeof b);
+const c ={
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+console.log (c);
+c ['friend'] = "Jane";
+delete c['age'];
+console.log(c) ;
