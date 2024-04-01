f971accf83c0579cc3d75171b8172ca16e18624d
Rakim Biswas <raktimb181@gmail.com>
2024-04-01 14:24:31 +0530

    Comment all

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
-/*console.log("hi");
-let ac = 20;
-let bc = " hi";
-console.log(ac + bc);
-{
-  const a = 50;
-  console.log(a); //this will show the value of const a which is 50
-}
-console.log(typeof bc);
-const c ={
-  name: 'John',
-  age: 30,
-  city: 'New York'
-};
-console.log (c);
-c ['friend'] = "Jane";
-delete c['age'];
-console.log(c) ;
-const Dictionary ={
-  appreciate:'Respect',
-  love :'Love',
-  peace :'Peace',
-  coward:'fear',
-  beaut̥iful:'pretty'
-}
-console.log("🚀 ~ Dictionary.beaut̥iful:", Dictionary.beaut̥iful)
-console.log(Dictionary['appreciate']);
-operators in js
-let a = 5;
-let b = 5;
-console.log("addition", a + b);
-console.log("subtraction", a - b);
-console.log("multiplication", a * b);
-console.log("division", a / b);
-console.log("modulus", a % b);
-console.log("increment", ++a);
-console.log("decrement", --a);
-console.log("idk", a++);
-console.log("idk too", a--); */
-const prompt = require('prompt-sync')();
-let a = prompt( "Enter your age:" );
-a = Number.parseInt(a);
-if (a>17){
-  console.log("You can apply for  voting");
-}
-else if(a<17 && a>9){
-console.log( "Your are not eligible to vote");
-}
-else {
-  console.log ("You are not old enough to vote");
-}
-console.log("you are a", a<10 ? 'child': a>12 && a<20 ? 'teenager': a>21?  'adult':'senior');
-// TODO do some tests
-/*
- *i'm checking the extensions...
-*/
-//TODO: fix this code
-//FIXME: fix this code
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-}
+// /*console.log("hi");
+// let ac = 20;
+// let bc = " hi";
+// console.log(ac + bc);
+// {
+//   const a = 50;
+//   console.log(a); //this will show the value of const a which is 50
+// }
+// console.log(typeof bc);
+// const c ={
+//   name: 'John',
+//   age: 30,
+//   city: 'New York'
+// };
+// console.log (c);
+// c ['friend'] = "Jane";
+// delete c['age'];
+// console.log(c) ;
+// const Dictionary ={
+//   appreciate:'Respect',
+//   love :'Love',
+//   peace :'Peace',
+//   coward:'fear',
+//   beaut̥iful:'pretty'
+// }
+// console.log("🚀 ~ Dictionary.beaut̥iful:", Dictionary.beaut̥iful)
+// console.log(Dictionary['appreciate']);
+// operators in js
+// let a = 5;
+// let b = 5;
+// console.log("addition", a + b);
+// console.log("subtraction", a - b);
+// console.log("multiplication", a * b);
+// console.log("division", a / b);
+// console.log("modulus", a % b);
+// console.log("increment", ++a);
+// console.log("decrement", --a);
+// console.log("idk", a++);
+// console.log("idk too", a--); */
+// const prompt = require('prompt-sync')();
+// let a = prompt( "Enter your age:" );
+// a = Number.parseInt(a);
+// if (a>17){
+//   console.log("You can apply for  voting");
+// }
+// else if(a<17 && a>9){
+// console.log( "Your are not eligible to vote");
+// }
+// else {
+//   console.log ("You are not old enough to vote");
+// }
+// console.log("you are a", a<10 ? 'child': a>12 && a<20 ? 'teenager': a>21?  'adult':'senior');
+// // TODO do some tests
+// /*
+//  *i'm checking the extensions...
+// */
+// //TODO: fix this code
+// //FIXME: fix this code
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
