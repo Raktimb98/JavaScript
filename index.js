21dbbac622368c946111602c015a69d2c29061c0
Raktim Biswas <raktimb181@gmail.com>
2025-02-22 15:20:52 +0530

    Do While Loop

--- index.js
+++ index.js
@@ -136,3 +136,13 @@
 //   i++;
 // }
 // console.log("hello");
+
+// Do While loop
+const prompt = require("prompt-sync")();
+let n = prompt("Enter the number:");
+n = Number.parseInt(n);
+let i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<n);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,22 @@
-let ac = 20;
-let bc = " hi";
-console.log(ac + bc);
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
