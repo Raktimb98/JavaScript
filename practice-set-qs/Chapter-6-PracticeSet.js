ab21f40117fc318c629be31a6fe2023271a08c3b
Raktim Biswas <raktimb181@gmail.com>
2025-04-22 15:02:04 +0530

    Practice set 6 end

--- practice-set-qs/Chapter-6-PracticeSet.js
+++ practice-set-qs/Chapter-6-PracticeSet.js
@@ -19,25 +19,72 @@
 
 //? QS NO 2 : Do the same as above but use the confirm to ask if the user wants to see the dialog box again or not.
 
-const canDrive = (age) => {
-  return age >= 18 ? true : false;
-};
+// const canDrive = (age) => {
+//   return age >= 18 ? true : false;
+// };
 
-let runAgain = true;
+// let runAgain = true;
 
-while (runAgain) {
-  let age = prompt("Enter your age:");
-  age = parseInt(age);
+// while (runAgain) {
+//   let age = prompt("Enter your age:");
+//   age = parseInt(age);
 
-  if (isNaN(age)) {
-    alert("Only numbers please!");
-  } else {
-    if (canDrive(age)) {
-      alert("You can drive");
-    } else {
-      alert("You cannot drive");
-    }
-  }
+//   if (isNaN(age)) {
+//     alert("Only numbers please!");
+//   } else {
+//     if (canDrive(age)) {
+//       alert("You can drive");
+//     } else {
+//       alert("You cannot drive");
+//     }
+//   }
 
-  runAgain = confirm("Do you want to check again?");
-}
+//   runAgain = confirm("Do you want to check again?");
+// }
+
+//? QS NO 3 : In the previous question use 'console.error' to log the error if the age entered is negative.
+
+// const canDrive = (age) => {
+//     return age >= 18 ? true : false;
+//   };
+  
+//   let runAgain = true;
+  
+//   while (runAgain) {
+//     let age = prompt("Enter your age:");
+//     age = parseInt(age);
+
+//     if (age < 0) {
+//       console.error("Age cannot be negative!");
+//       break;
+//     }
+  
+//     if (isNaN(age)) {
+//       alert("Only numbers please!");
+//     } else {
+//       if (canDrive(age)) {
+//         alert("You can drive");
+//       } else {
+//         alert("You cannot drive");
+//       }
+//     }
+  
+//     runAgain = confirm("Do you want to check again?");
+//   }
+
+//? QS NO 4 : Write a program to change the url to 'google.com' (Redirection) if user enters a number greater than 4.
+
+// let number = prompt("Enter a number:");
+// number = parseInt(number);
+
+// if(number > 4) {
+//     window.location.href = "https://www.google.com";
+// }
+// else {
+//     alert("Number is less than or equal to 4. No redirection.");
+// }
+
+//? QS NO 5 : Change the background of the page to 'yellow' , 'red' or any other color based on user input through prompt.
+
+let color = prompt("Enter the page background color (e.g., 'yellow', 'red', etc.):");
+document.body.style.backgroundColor = color;
