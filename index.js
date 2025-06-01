9858ba85601e323cc341dea723746bdcaf0ffcd3
Raktim Biswas <raktimb181@gmail.com>
2025-06-01 18:14:55 +0530

    Callback hell and Pyramid of doom

--- index.js
+++ index.js
@@ -468,15 +468,123 @@
 // }, 2000);
 
 //! Callback
-function loadScript(src,callback) {
-    var script = document.createElement("script");
-    script.src = src;
-    script.onload = function() {
-        console.log("Script loaded successfully");
-        if (callback) {
-            callback();
-        }
-    };
-    document.body.appendChild(script);
+// function loadScript(src,callback) {
+//     var script = document.createElement("script");
+//     script.src = src;
+//     script.onload = function() {
+//         console.log("Script loaded successfully");
+//         if (callback) {
+//             callback();
+//         }
+//     };
+//     document.body.appendChild(script);
+// }
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js")
+
+//* Callback Hell & Pyramid of Doom
+function loadScript(src, callback) {
+  var script = document.createElement("script");
+  script.src = src;
+  script.onload = function () {
+    console.log("Loaded script with SRC: " + src);
+    callback(null, src);
+  };
+  script.onerror = function () {
+    console.log("Error loading script with SRC: " + src);
+    callback(new Error("Src got some error"));
+  };
+  document.body.appendChild(script);
 }
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js")
+
+function hello(error, src) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  alert("Hello World!" + src);
+}
+//pyramid of doom
+loadScript(
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",
+  function goodmorning(error, src) {
+    if (error) {
+      console.log(error);
+      sendEmergencyMessageToCeo();
+      return;
+    }
+    loadScript(
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap2.bundle.min.js",
+      function goodmorning(error, src) {
+        if (error) {
+          console.log(error);
+          sendEmergencyMessageToCeo();
+          return;
+        }
+        loadScript(
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap3.bundle.min.js",
+          function goodmorning(error, src) {
+            if (error) {
+              console.log(error);
+              sendEmergencyMessageToCeo();
+              return;
+            }
+            loadScript(
+              "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap4.bundle.min.js",
+              function goodmorning(error, src) {
+                if (error) {
+                  console.log(error);
+                  sendEmergencyMessageToCeo();
+                  return;
+                }
+                loadScript(
+                  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap5.bundle.min.js",
+                  function goodmorning(error, src) {
+                    if (error) {
+                      console.log(error);
+                      sendEmergencyMessageToCeo();
+                      return;
+                    }
+                    loadScript(
+                      "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap6.bundle.min.js",
+                      function goodmorning(error, src) {
+                        if (error) {
+                          console.log(error);
+                          sendEmergencyMessageToCeo();
+                          return;
+                        }
+                        loadScript(
+                          "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap7.bundle.min.js",
+                          function goodmorning(error, src) {
+                            if (error) {
+                              console.log(error);
+                              sendEmergencyMessageToCeo();
+                              return;
+                            }
+                            loadScript(
+                              "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap8.bundle.min.js",
+                              function goodmorning(error, src) {
+                                if (error) {
+                                  console.log(error);
+                                  sendEmergencyMessageToCeo();
+                                  return;
+                                }
+                                loadScript(
+                                  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap9.bundle.min.js",
+                                  function goodmorning(error, src) {}
+                                );
+                              }
+                            );
+                          }
+                        );
+                      }
+                    );
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+);
